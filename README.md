f97c2bcd8069cf9e7b2e12b49c945102b333aade
mamurra5 <mamurra5@ncsu.edu>
2021-05-29 16:29:36 -0400

    Initial project setup

---
+++ README.md
@@ -0,0 +1,15 @@
+# anime_reference
+
+## Installing
+### Via `pip`
+I wrote this library to quickly get summaries from my favourite anime episodes. Hopefully, you find it easy to use.
+Install using the following command:
+
+```
+pip install anime-reference
+```
+
+### Via GitHub
+Alternatively, you can just clone this repo and import the libraries at your own discretion.
+
+### API
---
+++ setup.py
@@ -0,0 +1,36 @@
+# Author: Matt Murray
+# Description:
+
+from setuptools import setup
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(name='anime_reference',
+      version='0.0.3',
+      description='A reference package for all things anime.',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url='https://github.com/Matt711/anime_reference',
+      author='Matt Murray',
+      author_email='matthewmurray711@gmail.com',
+      license='MIT',
+      packages=['anime_reference'],
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent",
+      ],
+      python_requires=">=3.6",
+      install_requires=[
+          'beautifulsoup4==4.9.3',
+          'requests==2.25.1'
+      ],
+      keywords=[
+          "fandom anime",
+          "manga",
+          "data mining",
+          "anime",
+          "anime reference"
+      ],
+      zip_safe=False)
